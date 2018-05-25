97c75637b39fed67dc9b024befc94cd366125c73
Zia <zia@aimusic.co.uk>
2018-05-25 22:47:18 +0100

    Working react native view

--- ios/OscillatorApp/ViewController.m
+++ ios/OscillatorApp/ViewController.m
@@ -21,6 +21,15 @@
 {
   [super viewDidLoad];
   gst_backend = [[GStreamerBackend alloc] init:self];
+  NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.bundle?platform=ios"];
+  RCTRootView *rootView =
+  [[RCTRootView alloc] initWithBundleURL: jsCodeLocation
+                              moduleName: @"OscillatorApp"
+                       initialProperties: @{}
+                           launchOptions: nil];
+  
+  self.view = rootView;
+
 }
 
 - (void)didReceiveMemoryWarning {
